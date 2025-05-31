5f5175f319103c35530809edaa3f5d837a1cd634
Jabari <jabari.xa.barton@gmail.com>
2025-05-31 14:14:54 -0700

    Added more error handling for recording_voice_client.py

--- audio_management/recording_voice_client.py
+++ audio_management/recording_voice_client.py
@@ -1,4 +1,7 @@
-from discord import VoiceClient
+import traceback
+
+from discord import VoiceClient, ClientException
+from discord.sinks import RecordingException
 
 
 class RecordingVoiceClient(VoiceClient):
@@ -9,9 +12,38 @@ class RecordingVoiceClient(VoiceClient):
         self.callback = callback
 
     async def connect(self, *args, **kwargs):
-        # Await the original connect method from the parent class
-        voice_client = await super().connect(*args, **kwargs)
-        print("Starting recording in voice channel")
-        self.start_recording(self.sink, self.callback, self.channel)
+        # 60 sec timeout is too long
+        kwargs['timeout'] = 30.0
 
-        return voice_client
+        print(f"Attempting connection to voice channel - Server ID: {self.server_id}")
+        try:
+            await super().connect(*args, **kwargs)
+            if self.is_connected():
+                print(f"Starting recording in voice channel - Server ID: {self.server_id}")
+                if not self.recording:
+                    self.start_recording(self.sink, self.callback, self.channel)
+            else:
+                print("Connection attempted but bot is still not connected to voice channel. Disconnecting...")
+                await self.disconnect(force=True)
+        except ClientException as e:
+            print(f"ClientException during connection (possibly reconnection). {e} - Server ID: {self.server_id}")
+
+            if self.recording:
+                self.stop_recording()
+            await self.disconnect(force=True)
+        except RecordingException as e:
+            print(f"RecordingException during connection (possibly reconnection). {e} - Server ID: {self.server_id}")
+        except TimeoutError as e:
+            print(f"TimeoutError during connection (possibly reconnection). {e} - Server ID: {self.server_id}")
+            if self.recording:
+                self.stop_recording()
+            await self.disconnect(force=True)
+        except Exception as e:
+            stack_trace = traceback.format_exc()
+            print(f"Failed to connect to voice channel in RecordingVoiceClient: {type(e).__name__} - {e}\nStack trace:\n{stack_trace} - Server ID: {self.server_id}")
+            if self.recording:
+                self.stop_recording()
+            await self.disconnect(force=True)
+
+    async def edit(self, **kwargs):
+        return await super().edit(**kwargs)
